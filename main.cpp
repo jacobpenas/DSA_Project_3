11bc47624336cb907cf9aa0026832239a9a15b1e
jacobpenas <92338651+jacobpenas@users.noreply.github.com>
2023-12-04 02:46:31 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -25,102 +25,113 @@ int main() {
 	AdjacencyList graph;
 	graph.loadGraph("twitch.txt");
 	int start, destination;
-	string input1, input2;
-	bool valid1 = true, valid2 = true;
+	string loop = "y";
 
-// input validation for starting node
-	while (true) {
-		cout << "Please enter a start ID (ranging from 0 to 168112): ";
-		getline(cin, input1);
-		for (int i = 0; i < input1.length(); i++) {
-			if (!isdigit(input1[i]))
-				valid1 = false;
+	while (loop == "y") {
+
+		string input1, input2;
+
+		// input validation for starting node
+		while (true) {
+			bool valid1 = true;
+			cout << "Please enter a start ID (ranging from 0 to 168112): ";
+			getline(cin, input1);
+			for (int i = 0; i < input1.length(); i++) {
+				if (!isdigit(input1[i]))
+					valid1 = false;
+			}
+			if (valid1 && stoi(input1) >= 0 && stoi(input1) <= 168112) {
+				start = stoi(input1);
+				break;
+			}
+			cout << "Start ID invalid. Please try again." << endl;
 		}
-		if (valid1 && stoi(input1) >= 0 && stoi(input1) <= 168112) {
-			start = stoi(input1);
-			break;
+
+		// input validation for target node
+		while (true) {
+			bool valid2 = true;
+			cout << "Please enter a destination ID (ranging from 0 to 168112): ";
+			getline(cin, input2);
+			for (int i = 0; i < input2.length(); i++) {
+				if (!isdigit(input2[i]))
+					valid2 = false;
+			}
+			if (valid2 && stoi(input2) >= 0 && stoi(input2) <= 168112) {
+				destination = stoi(input2);
+				break;
+			}
+			cout << "Destination ID invalid. Please try again." << endl;
 		}
-		cout << "Start ID invalid. Please try again." << endl;
-	}
 
-// input validation for target node
-	while (true) {
-		cout << "Please enter a destination ID (ranging from 0 to 168112): ";
-		getline(cin, input2);
-		for (int i = 0; i < input2.length(); i++) {
-			if (!isdigit(input2[i]))
-				valid2 = false;
+		unordered_map<int, bool> visited;
+		int bfsNodes = 0, dfsNodes = 0;
+
+		// performs bfs, tracks time elapsed
+		auto bfsStart = chrono::high_resolution_clock::now();
+		bool bfsSearch = graph.bfs(start, destination, visited, bfsNodes);
+		auto bfsDone = chrono::high_resolution_clock::now();
+		auto bfsTime = chrono::duration_cast<chrono::milliseconds>(bfsDone - bfsStart);
+
+		visited.clear();
+
+		// performs dfs, tracks time elapsed
+		auto dfsStart = chrono::high_resolution_clock::now();
+		bool dfsSearch = graph.dfs(start, destination, visited, dfsNodes);
+		auto dfsDone = chrono::high_resolution_clock::now();
+		auto dfsTime = chrono::duration_cast<chrono::milliseconds>(dfsDone - dfsStart);
+
+		// output bfs results
+		cout << endl << "BFS:" << endl;
+		if (bfsSearch) {
+			cout << "\tDestination found!" << endl;
 		}
-		if (valid2 && stoi(input2) >= 0 && stoi(input2) <= 168112) {
-			destination = stoi(input2);
-			break;
+		else {
+			cout << "\tDesination not found." << endl;
 		}
-		cout << "Destination ID invalid. Please try again." << endl;
-	}
+		cout << "\tTime elapsed: " << bfsTime.count() << " milliseconds" << endl
+			<< "\tNumber of nodes iterated through: " << bfsNodes << endl << endl;
 
-	unordered_map<int, bool> visited;
-	int bfsNodes = 0, dfsNodes = 0;
+		// output dfs results
+		cout << "DFS:" << endl;
+		if (dfsSearch) {
+			cout << "\tDestination found!" << endl;
+		}
+		else {
+			cout << "\tDesination not found." << endl;
+		}
+		cout << "\tTime elapsed: " << dfsTime.count() << " milliseconds" << endl
+			<< "\tNumber of nodes iterated through: " << dfsNodes << endl << endl;
 
-// performs bfs, tracks time elapsed
-	auto bfsStart = chrono::high_resolution_clock::now();
-	bool bfsSearch = graph.bfs(start, destination, visited, bfsNodes);
-	auto bfsDone = chrono::high_resolution_clock::now();
-	auto bfsTime = chrono::duration_cast<chrono::milliseconds>(bfsDone - bfsStart);
+		// outputs time comparison between bfs and dfs
+		if (bfsTime.count() < dfsTime.count()) {
+			cout << "BFS completed the search " << dfsTime.count() - bfsTime.count()
+				<< " milliseconds quicker than DFS" << endl;
+		}
+		else if (bfsTime.count() > dfsTime.count()) {
+			cout << "DFS completed the search " << bfsTime.count() - dfsTime.count()
+				<< " milliseconds quicker than BFS" << endl;
+		}
+		else {
+			cout << "BFS and DFS took the same amount of time" << endl;
+		}
 
-	visited.clear();
-	
-// performs dfs, tracks time elapsed
-	auto dfsStart = chrono::high_resolution_clock::now();
-	bool dfsSearch = graph.dfs(start, destination, visited, dfsNodes);
-	auto dfsDone = chrono::high_resolution_clock::now();
-	auto dfsTime = chrono::duration_cast<chrono::milliseconds>(dfsDone - dfsStart);
+		// outputs comparison between number of nodes passed through
+		if (bfsNodes < dfsNodes) {
+			cout << "BFS completed the search in " << dfsNodes - bfsNodes
+				<< " fewer node visitations than DFS" << endl;
+		}
+		else if (bfsNodes > dfsNodes) {
+			cout << "DFS completed the search in " << bfsNodes - dfsNodes
+				<< " fewer node visitations than BFS" << endl;
+		}
+		else
+			cout << "BFS and DFS completed the search with the same number of node visitations." << endl;
 
-// output bfs results
-	cout << endl << "BFS:" << endl;
-	if (bfsSearch) {
-		cout << "\tDestination found!" << endl;
-	}
-	else {
-		cout << "\tDesination not found." << endl;
-	}
-	cout << "\tTime elapsed: " << bfsTime.count() << " milliseconds" << endl
-		<< "\tNumber of nodes iterated through: " << bfsNodes << endl << endl;
+		cout << endl << "Would you like to continue? Input 'y' to continue or any other character to quit.\n";
+		getline(cin, loop);
+		cout << endl << endl;
 
-// output dfs results
-	cout << "DFS:" << endl;
-	if (dfsSearch) {
-		cout << "\tDestination found!" << endl;
 	}
-	else {
-		cout << "\tDesination not found." << endl;
-	}
-	cout << "\tTime elapsed: " << dfsTime.count() << " milliseconds" << endl
-		<< "\tNumber of nodes iterated through: " << dfsNodes << endl << endl;
-
-// outputs time comparison between bfs and dfs
-	if (bfsTime.count() < dfsTime.count()) {
-		cout << "BFS completed the search " << dfsTime.count() - bfsTime.count()
-			<< " milliseconds quicker than DFS" << endl;
-	}
-	else if (bfsTime.count() > dfsTime.count()) {
-		cout << "DFS completed the search " << bfsTime.count() - dfsTime.count()
-			<< " milliseconds quicker than BFS" << endl;
-	}
-	else {
-		cout << "BFS and DFS took the same amount of time" << endl;
-	}
-
-// outputs comparison between number of nodes passed through
-	if (bfsNodes < dfsNodes) {
-		cout << "BFS completed the search in " << dfsNodes - bfsNodes
-			<< " fewer node visitations than DFS" << endl;
-	}
-	else if (bfsNodes > dfsNodes) {
-		cout << "DFS completed the search in " << bfsNodes - dfsNodes
-			<< " fewer node visitations than BFS" << endl;
-	}
-	else
-		cout << "BFS and DFS completed the search with the same number of node visitations." << endl;
 
 	return 0;
 }
